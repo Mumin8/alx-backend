c838d217aea6fc5c2f5b41c7eee24514d6e3146d
Mumin8 <alhassanmumin8@gmail.com>
2024-03-31 11:26:25 +0000

    pagination

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,31 +39,39 @@ class Server:
             }
         return self.__indexed_dataset
 
-    def get_hyper_index(self, index: int = None,
-                        page_size: int = 10) -> Dict:
-        """ return all data"""
+    def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Return a dictionary containing hypermedia index information for a given index or page number.
+
+        Args:
+            index (int): The index of the item to retrieve.
+            page_size (int): The number of items per page.
+
+        Returns:
+            Dict: A dictionary containing hypermedia index information.
+        """
+        dataset_len = len(self.dataset())
+        total_pages = math.ceil(dataset_len / page_size)
 
         if index is None:
-            index = 0
+            return {
+                "index": None,
+                "page_size": page_size,
+                "current_page": None,
+                "total_pages": total_pages,
+                "items": {}
+            }
 
-        # validate the index
-        assert isinstance(index, int)
-        assert 0 <= index < len(self.indexed_dataset())
-        assert isinstance(page_size, int) and page_size > 0
+        current_page = index // page_size + 1
+        start_index = (current_page - 1) * page_size
+        end_index = min(start_index + page_size, dataset_len)
 
-        data = []  # collect all indexed data
-        next_index = index + page_size
-
-        for value in range(index, next_index):
-            if self.indexed_dataset().get(value):
-                data.append(self.indexed_dataset()[value])
-            else:
-                value += 1
-                next_index += 1
+        items = {i: self.dataset()[i] for i in range(start_index, end_index)}
 
         return {
-            'index': index,
-            'data': data,
-            'page_size': page_size,
-            'next_index': next_index
+            "index": index,
+            "page_size": page_size,
+            "current_page": current_page,
+            "total_pages": total_pages,
+            "items": items
         }
